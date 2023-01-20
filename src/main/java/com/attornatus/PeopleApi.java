4eecf69bf8021012a2448b93476281f6bfe8ad12
Willian Carlos <wcs777git@outlook.com>
2023-01-20 19:58:57 -0300

    change people to PeopleApi

--- src/main/java/com/attornatus/PeopleApi.java
+++ src/main/java/com/attornatus/PeopleApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class people {
+public class PeopleApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(people.class, args);
+		SpringApplication.run(PeopleApi.class, args);
 	}
 
 }
