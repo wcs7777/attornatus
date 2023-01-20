27fa3cce8903674ae8af7c1e209e927af991f95f
Willian Carlos <wcs777git@outlook.com>
2023-01-20 20:00:13 -0300

    refactor HomeController

--- src/main/java/com/attornatus/controller/HomeController.java
+++ src/main/java/com/attornatus/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.attornatus.controller;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +11,10 @@ public class HomeController {
 
 	@Value("${home.message}")
 	private String message = "home";
-	private Map<String, String> json = new ConcurrentHashMap<>();
-
-	public HomeController() {
-		this.json.put("message", this.message);
-	}
 
 	@GetMapping("/")
 	public Map<String, String> home() {
-		return this.json;
+		return Map.of("message", this.message);
 	}
 
 }
